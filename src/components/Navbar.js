528293e5fe395bae7309494eebbda5b261f4bafe
ashish-opengov <achougule@opengov.com>
2023-07-19 18:42:58 +0530

    Added Navbar component

---
+++ src/components/Navbar.js
@@ -0,0 +1,93 @@
+import React from "react";
+import PropTypes from 'prop-types';
+
+export default function Navbar(props) {
+  return (
+    <nav
+      className="navbar navbar-expand-lg navbar-light bg-dark"
+      data-bs-theme="dark"
+    >
+      <a className="navbar-brand" href="/">
+        <img
+          src="https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo.svg"
+          alt="Logo"
+          width="30"
+          height="24"
+          className="d-inline-block align-text-top"
+        />
+        {props.title}
+      </a>
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon"></span>
+      </button>
+
+      <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul className="navbar-nav mr-auto">
+          <li className="nav-item active">
+            <a className="nav-link" href="/">
+              Home <span className="sr-only">(current)</span>
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="/">
+              Link
+            </a>
+          </li>
+          <li className="nav-item dropdown">
+            <a
+              className="nav-link dropdown-toggle"
+              href="/"
+              role="button"
+              data-bs-toggle="dropdown"
+              aria-expanded="false"
+            >
+              Dropdown link
+            </a>
+            <ul className="dropdown-menu">
+              <li>
+                <a className="dropdown-item" href="/">
+                  Action
+                </a>
+              </li>
+              <li>
+                <a className="dropdown-item" href="/">
+                  Another action
+                </a>
+              </li>
+              <li>
+                <a className="dropdown-item" href="/">
+                  Something else here
+                </a>
+              </li>
+            </ul>
+          </li>
+        </ul>
+
+        <form className="d-flex" role="search">
+          <input
+            className="form-control me-2"
+            type="search"
+            placeholder="Search"
+            aria-label="Search"
+          />
+          <button className="btn btn-outline-success" type="submit">
+            Search
+          </button>
+        </form>
+      </div>
+    </nav>
+  );
+}
+
+Navbar.protoTypes = {
+  title: PropTypes.string.isRequired,
+  aboutText: PropTypes.string.isRequired
+};
